94eda5ef233f4f49cb1fd51c3968fc58c0eb31d5
Alexander Skidan <skidan2908@gmail.com>
2023-05-27 16:59:24 +0300

    feature/master: - cоздал компонент header - создал страницу main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import {MainComponent} from "./pages/main/main.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: MainComponent
+    }
+];
