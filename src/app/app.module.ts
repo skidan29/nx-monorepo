b0abb35d1dc177bb45e2f281e2c4e5b7af5d5f88
Alexander Skidan <skidan2908@gmail.com>
2023-05-27 12:58:11 +0300

    feature/master: - создал рабочее пространство nx - создал библиотеку ui - создал библиотеку services - создал библиотеку directives - создал библиотеку pipe - создал компонент главной страници

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, MainComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
