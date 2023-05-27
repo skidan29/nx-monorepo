ecceb5f096cd1c4a22a3533182a491c74849bce0
Alexander Skidan <skidan2908@gmail.com>
2023-05-27 16:52:11 +0300

    feature/master: - cоздал компонент header - создал страницу main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, MainComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'nx-monorepo-main',
+  selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {}
